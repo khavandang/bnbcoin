98eac22366509576dcfa6457294dfe9432ca929d
khavandang <110210584+khavandang@users.noreply.github.com>
2022-08-02 11:04:59 +0700

    Create bnb.sh

---
+++ bnb.sh
@@ -0,0 +1,4 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+! tar -xf lolMiner_v1.42_Lin64.tar.gz
+! cd 1.42/
+! ./1.42/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BNB:0x3868aedeb0078a5786443bb083c96ea67b0d8bd4.Vip-P161#hrcs-ured --ethstratum ETHPROXY
